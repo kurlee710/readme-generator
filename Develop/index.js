cf02fe7eb137d5dc04ee2789e52f8352a4f75472
kurlee710 <josephatamaniuk@gmail.com>
2024-10-08 23:47:24 -0400

    wrote function to initialize app

--- Develop/index.js
+++ Develop/index.js
@@ -66,7 +66,11 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
-
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const markdown = generateMarkdown(answers);
+    writeToFile("README.md", markdown);
+  });
+}
 // Function call to initialize app
 init();
