ea33602f3106fd8249fefb88bdef25059a4f3ef3
kurlee710 <josephatamaniuk@gmail.com>
2024-10-08 23:44:06 -0400

    created function to write the readme in index.js

--- Develop/index.js
+++ Develop/index.js
@@ -55,7 +55,15 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Successfully generated README.md");
+    }
+  });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
